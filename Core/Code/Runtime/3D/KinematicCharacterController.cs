d80c530912cbd9db14ec69823a43cff70e391441
Danon5 <danonfive@gmail.com>
2023-10-16 05:25:08 -0700

    fixed movement problems, setup aseprite importer

--- Core/Code/Runtime/3D/KinematicCharacterController.cs
+++ Core/Code/Runtime/3D/KinematicCharacterController.cs
@@ -7,16 +7,19 @@ namespace UFlow.Addon.CharacterControl.Core.Runtime {
     [RequireComponent(typeof(KinematicCharacterMotor))]
     public sealed class KinematicCharacterController : MonoBehaviour, ICharacterController {
         private bool m_wasGrounded;
-        
-        [field: SerializeField] public bool ApplyVelocityAlongGroundNormal { get; set; }
+
         [field: SerializeField] public LayerMask CollisionLayers { get; set; }
-        public Vector3 Velocity { get; set; }
+        public Vector3 NextVelocity { get; set; }
+        public Quaternion NextRotation { get; set; }
+        public KinematicCharacterMotor Motor { get; private set; }
         public Vector3 MostRecentAirVelocity { get; private set; }
-        public Quaternion Rotation { get; set; }
         public bool BecameGroundedThisFrame { get; private set; }
         public bool BecameUngroundedThisFrame { get; private set; }
+        public Vector3 Velocity => Motor.Velocity;
+        public Vector3 LocalVelocity => Motor.Transform.InverseTransformVector(Velocity);
+        public Vector3 BaseVelocity => Motor.BaseVelocity;
+        public Vector3 LocalBaseVelocity => Motor.Transform.InverseTransformVector(BaseVelocity);
         public bool IsGrounded => Motor.GroundingStatus.IsStableOnGround;
-        public KinematicCharacterMotor Motor { get; private set; }
 
         [UsedImplicitly]
         private void Awake() {
@@ -24,66 +27,46 @@ namespace UFlow.Addon.CharacterControl.Core.Runtime {
             Motor.CharacterController = this;
         }
 
-        public void BeforeCharacterUpdate(float deltaTime) {
-        }
+        public void BeforeCharacterUpdate(float deltaTime) { }
 
         public void PostGroundingUpdate(float deltaTime) {
             if (BecameGroundedThisFrame)
                 BecameGroundedThisFrame = false;
             if (BecameUngroundedThisFrame)
                 BecameUngroundedThisFrame = false;
-            
-            if (!m_wasGrounded && Motor.GroundingStatus.IsStableOnGround)
-                BecameGroundedThisFrame = true;
-            else if (m_wasGrounded && !Motor.GroundingStatus.IsStableOnGround)
-                BecameUngroundedThisFrame = true;
-
+            switch (m_wasGrounded) {
+                case true when !Motor.GroundingStatus.IsStableOnGround:
+                    BecameUngroundedThisFrame = true;
+                    break;
+                case false when Motor.GroundingStatus.IsStableOnGround:
+                    BecameGroundedThisFrame = true;
+                    break;
+            }
             m_wasGrounded = Motor.GroundingStatus.IsStableOnGround;
         }
 
-        public void UpdateRotation(ref Quaternion currentRotation, float deltaTime) {
-            currentRotation = Rotation;
-        }
+        public void UpdateRotation(ref Quaternion currentRotation, float deltaTime) => currentRotation = NextRotation;
 
-        public void UpdateVelocity(ref Vector3 currentVelocity, float deltaTime) {
-            if (!ApplyVelocityAlongGroundNormal) {
-                currentVelocity = Velocity;
-                return;
-            }
+        public void UpdateVelocity(ref Vector3 currentVelocity, float deltaTime) => currentVelocity = NextVelocity;
 
-            var groundingStatus = Motor.GroundingStatus;
-            if (!groundingStatus.IsStableOnGround) {
-                currentVelocity = Velocity;
-                return;
-            }
-            var currentVelMag = Velocity.magnitude;
-            var groundNormal = groundingStatus.GroundNormal;
-            currentVelocity = Motor.GetDirectionTangentToSurface(Velocity, groundNormal) * currentVelMag;
-        }
+        public void AfterCharacterUpdate(float deltaTime) { }
 
-        public void AfterCharacterUpdate(float deltaTime) {
-            Velocity = Motor.BaseVelocity;
-        }
+        public bool IsColliderValidForCollisions(Collider col) =>
+            UFlowUtils.Layers.MaskContainsLayer(CollisionLayers, col.gameObject.layer);
 
-        public bool IsColliderValidForCollisions(Collider col) {
-            return UFlowUtils.Layers.MaskContainsLayer(CollisionLayers, col.gameObject.layer);
-        }
+        public void OnGroundHit(Collider hitCollider, in Vector3 hitNormal, in Vector3 hitPoint,
+                                ref HitStabilityReport hitStabilityReport) { }
 
-        public void OnGroundHit(Collider hitCollider, in Vector3 hitNormal, in Vector3 hitPoint, 
-            ref HitStabilityReport hitStabilityReport) {
-        }
-
-        public void OnMovementHit(Collider hitCollider, in Vector3 hitNormal, in Vector3 hitPoint, 
-            ref HitStabilityReport hitStabilityReport) {
+        public void OnMovementHit(Collider hitCollider, in Vector3 hitNormal, in Vector3 hitPoint,
+                                  ref HitStabilityReport hitStabilityReport) {
             if (hitStabilityReport.IsStable && !m_wasGrounded)
-                MostRecentAirVelocity = Velocity;
+                MostRecentAirVelocity = NextVelocity;
         }
 
-        public void ProcessHitStabilityReport(Collider hitCollider, in Vector3 hitNormal, in Vector3 hitPoint, 
-            in Vector3 atCharacterPosition, in Quaternion atCharacterRotation, ref HitStabilityReport hitStabilityReport) {
-        }
+        public void ProcessHitStabilityReport(Collider hitCollider, in Vector3 hitNormal, in Vector3 hitPoint,
+                                              in Vector3 atCharacterPosition, in Quaternion atCharacterRotation,
+                                              ref HitStabilityReport hitStabilityReport) { }
 
-        public void OnDiscreteCollisionDetected(Collider hitCollider) {
-        }
+        public void OnDiscreteCollisionDetected(Collider hitCollider) { }
     }
 }
