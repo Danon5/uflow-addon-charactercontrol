520d243d7f913eb34f854ff651da273ca8c7fa4c
Danon5 <danonfive@gmail.com>
2024-09-27 07:26:08 -0700

    fixed GUI errors when no capsule selected

--- Plugins/KinematicCharacterController/Core/Editor/KinematicCharacterMotorEditor.cs
+++ Plugins/KinematicCharacterController/Core/Editor/KinematicCharacterMotorEditor.cs
@@ -9,18 +9,17 @@ namespace KCC.Editor
         protected virtual void OnSceneGUI()
         {            
             CharacterMotor motor = (target as CharacterMotor);
-            if (motor)
-            {
-                Vector3 characterBottom = motor.transform.position + (motor.Capsule.center + (-Vector3.up * (motor.Capsule.height * 0.5f)));
+            if (!motor) return;
+            if (!motor.Capsule) return;
+            Vector3 characterBottom = motor.transform.position + (motor.Capsule.center + (-Vector3.up * (motor.Capsule.height * 0.5f)));
 
-                Handles.color = Color.yellow;
-                Handles.CircleHandleCap(
-                    0, 
-                    characterBottom + (motor.transform.up * motor.MaxStepHeight), 
-                    Quaternion.LookRotation(motor.transform.up, motor.transform.forward), 
-                    motor.Capsule.radius + 0.1f, 
-                    EventType.Repaint);
-            }
+            Handles.color = Color.yellow;
+            Handles.CircleHandleCap(
+                0, 
+                characterBottom + (motor.transform.up * motor.MaxStepHeight), 
+                Quaternion.LookRotation(motor.transform.up, motor.transform.forward), 
+                motor.Capsule.radius + 0.1f, 
+                EventType.Repaint);
         }
     }
 }
